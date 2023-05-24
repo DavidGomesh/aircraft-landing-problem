becc6f1e5f30b47a06a38cfe60c3f6223dc02832
marcellodinizr <marcellodinizrocha@gmail.com>
2023-05-24 14:25:32 +0100

    Feat: Create ClassType enum

---
+++ src/main/scala/pj/enumerate/ClassType.scala
@@ -0,0 +1,31 @@
+package enumerate
+
+import ClassType.*
+import scala.annotation.targetName
+
+enum ClassType:
+    case Class1, Class2, Class3, Class4, Class5, Class6
+
+extension (c: ClassType)
+    def separation(t: ClassType): Int = (c, t) match
+        case (Class1, Class1) => 82
+        case (Class1, Class2) => 69
+        case (Class1, Class3) => 60
+        case (Class1, _)      => 75
+
+        case (Class2, Class1) => 131
+        case (Class2, Class2) => 69
+        case (Class2, Class3) => 60
+        case (Class2, _)      => 75
+
+        case (Class3, Class1) => 196
+        case (Class3, Class2) => 157
+        case (Class3, Class3) => 96
+        case (Class3, _)      => 75
+
+        case (Class4, _) => 60
+        case (Class5, _) => 60
+
+        case (Class6, Class1 | Class2 | Class3) => 60
+        case (Class6, Class4 | Class5) => 120
+        case (Class6, _) => 90
