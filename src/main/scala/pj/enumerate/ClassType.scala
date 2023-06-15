67c5def6d43d7401c84627c18ca8e04cb691ecaa
David Gomesh <davidgomesh2016@gmail.com>
2023-06-15 18:46:36 +0100

    Feat: Create AgendaIO

--- src/main/scala/pj/enumerate/ClassType.scala
+++ src/main/scala/pj/enumerate/ClassType.scala
@@ -2,6 +2,8 @@ package enumerate
 
 import ClassType.*
 import simpleTypes.integer.NonNegativeInt
+import pj.domain.Result
+import pj.domain.DomainError.*
 
 enum ClassType:
     case Class1, Class2, Class3, Class4, Class5, Class6
@@ -10,6 +12,17 @@ object ClassType:
     def LANDING_PENALTY = 2
     def TAKEOFF_PENALTY = 1
 
+    def apply(s: String): ClassType = toClass(s)
+
     def cost(c: ClassType, delay: NonNegativeInt): Int = c match
         case Class1 | Class2 | Class3 => delay * LANDING_PENALTY
         case Class4 | Class5 | Class6 => delay * TAKEOFF_PENALTY
+    
+    def toClass(s: String): ClassType = s match
+        case "1" => Class1
+        case "2" => Class2
+        case "3" => Class3
+        case "4" => Class4
+        case "5" => Class5
+        case "6" => Class6
+    
---
+++ src/main/scala/pj/io/AgendaIO.scala
@@ -0,0 +1,41 @@
+package pj.io
+
+import pj.domain.Result
+import domain.Agenda
+import scala.xml.Elem
+import pj.domain.DomainError.*
+import scala.xml.Node
+import simpleTypes.integer.NonNegativeInt
+import domain.Aircraft
+import pj.xml.XML.fromAttribute
+import enumerate.ClassType
+import domain.Runway
+import pj.xml.XML.traverse
+
+object AgendaIO:
+
+    def apply(path: String): Result[Agenda] = load(path)
+
+    def load(path: String) = 
+        for
+            file <- FileIO.load(path)
+            la <- traverse((file\"aircrafts"\"aircraft"), xmlToAircraft(_, 900))
+            lr <- traverse((file\"runways"\"runway"), xmlToRunway(_))
+            a <- Agenda.from(la, lr)
+        yield a
+
+    def xmlToAircraft(xml: Node, maxDelay: NonNegativeInt): Result[Aircraft] =
+        for
+            id <- fromAttribute(xml, "id")
+            c  <- fromAttribute(xml, "class").map(ClassType(_))
+            t  <- fromAttribute(xml, "target").map(_.toInt)
+            e  <- fromAttribute(xml, "emergency").fold(
+                e => Right(t + maxDelay), e => Right(t + e.toInt)
+            )
+        yield Aircraft(id, c, t, e)
+
+    def xmlToRunway(xml: Node): Result[Runway] = 
+        for
+            id <- fromAttribute(xml, "id")
+            lc <- traverse((xml\"handles"), h => fromAttribute(h, "class").map(ClassType(_)))
+        yield Runway(id, lc)
