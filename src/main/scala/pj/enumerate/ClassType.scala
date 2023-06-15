a3ddd2aa2652b17926792e49dead6b57cdff80ab
David Gomesh <davidgomesh2016@gmail.com>
2023-06-15 15:53:56 +0100

    Feat: Add domain functions

--- src/main/scala/pj/enumerate/ClassType.scala
+++ src/main/scala/pj/enumerate/ClassType.scala
@@ -1,4 +1,15 @@
 package enumerate
 
+import ClassType.*
+import simpleTypes.integer.NonNegativeInt
+
 enum ClassType:
     case Class1, Class2, Class3, Class4, Class5, Class6
+    
+object ClassType:
+    def LANDING_PENALTY = 2
+    def TAKEOFF_PENALTY = 1
+
+    def cost(c: ClassType, delay: NonNegativeInt): Int = c match
+        case Class1 | Class2 | Class3 => delay * LANDING_PENALTY
+        case Class4 | Class5 | Class6 => delay * TAKEOFF_PENALTY
